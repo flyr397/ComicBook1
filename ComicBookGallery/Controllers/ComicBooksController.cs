447fb020d9bee7569f242084bb49b3b5deb7a731
flyr397 <flyr397@gmail.com>
2017-03-26 21:02:11 -0400

    updated detail action method to use actionresult types

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -9,5 +9,14 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+
+            return Content("Hello from the comic boos controller");
+        }
     }
 }
